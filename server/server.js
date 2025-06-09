3a8216e102455e4db30649ad5983b6786fee6355
Niveditha072 <nivedithad07@gmail.com>
2025-06-09 19:35:53 +0530

    gitignores

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Task = require('./models/Task');
-require('.env').config();
+
 
 const app = express();
 app.use(cors());
